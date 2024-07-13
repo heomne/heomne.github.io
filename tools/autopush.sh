bed033fe266a031f97e2b5fb850af1fc3bcd2150
heomne <hmin4957@naver.com>
2024-07-13 23:40:03 +0900

    Automated commit on Sat Jul 13 23:40:03 KST 2024

---
+++ tools/autopush.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# heomne.github.io repository path
+REPO_PATH=/home/h_min/heomne.github.io/
+COMMIT_MSG="Automated commit on $(date)"
+
+# move to repository path
+cd "$REPO_PATH" || exit
+
+# Check untracked files
+CHK_UNTRACKED=$(git status --short --untracked)
+
+# Commit & Push
+if [ -n "$CHK_UNTRACKED" ]; then
+  git add .
+  git commit -m "$COMMIT_MSG"
+  git push origin main
+
+  # error
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to push changes to the repository"
+    exit 1
+  fi
+  echo "Successfully pushed changes to the repository"
+else
+  echo "No changes to commit"
+fi
