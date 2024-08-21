439e10061c20c8fabc470b0da1c04df7353fbb4e
heomne <hmin4957@naver.com>
2024-08-21 23:30:01 +0900

    Automated commit on Wed Aug 21 11:30:01 PM KST 2024

--- tools/img2webp.sh
+++ tools/img2webp.sh
@@ -1,17 +1,35 @@
-DIR_PATH=$(realpath "$1")
-if [ -z "$1" ]; then 
+DIR_NAME=$(ls -rtl ../_posts/ | tail -n 1 | awk -F " " '{print $9}' | awk -F "-" '{for(i=4; i<=NF; i++) printf $i"-"}')
+DIR_NAME=$(echo "${DIR_NAME::-4}")
+
+DIR_PATH="../assets/post_img/${DIR_NAME}"
+
+
+if [ -z "${DIR_PATH}" ]; then 
+
 	echo "Error: no path provided"
 	exit 1
 	
-	else
-	echo $DIR_PATH
+else
+
+	echo "DIR_PATH: "$DIR_PATH
+
 fi
 
-PNGS=$(ls -rtl $DIR_PATH | grep .png | awk -F " " '{print $9}' | awk -F "." '{print $1}')
-echo $PNGS
 
-for PNG in $PNGS
-do
-	cwebp -q 80 "${DIR_PATH}/${PNG}.png" -o "${DIR_PATH}/${PNG}.webp"
-done
+PNGS_CNT=$(ls -rtl $DIR_PATH | grep .png | wc -l)
+
+if [ "$PNGS_CNT" -eq 0 ]; then
+
+	echo "No PNG files found. Exiting."
+	exit 0
+
+else 
+
+	PNGS=$(ls -rtl $DIR_PATH | grep .png | awk -F " " '{print $9}' | awk -F "." '{print $1}')
+
+	for PNG in $PNGS
+	do
+		cwebp -q 80 "${DIR_PATH}/${PNG}.png" -o "${DIR_PATH}/${PNG}.webp"
+	done
+fi
 
